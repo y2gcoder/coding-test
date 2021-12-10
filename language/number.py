b63dcc791f5d85b74c8c5ddc5cf8cc67f6dda405
y2gcoder <y2gcoder@gmail.com>
2021-12-10 23:50:55 +0900

    Learning: Number

---
+++ language/number.py
@@ -0,0 +1,73 @@
+# 수 자료형
+# 정수형
+a = 1000    # 양의 정수
+print(a)
+
+a = -7      # 음의 정수
+print(a)
+
+# 0
+a = 0      # 음의 정수
+print(a)
+
+# 실수형
+# 양의 실수
+a = 157.93
+print(a)
+
+# 음의 실수
+a = -1837.2
+print(a)
+
+# 소수부가 0일 때 0을 생략
+a = 5.
+print(a)
+
+# 정수부가 0일 때 0을 생략
+a = -.7
+print(a)
+
+# 10억의 지수 표현 방식
+a = 1e9
+print(a)
+
+# 752.5
+a = 75.25e1
+print(a)
+
+# 3.954
+a = 3954e-3
+print(a)
+
+a = 0.3 + 0.6
+print(a)
+
+if a == 0.9:
+    print(True)
+else:
+    print(False)
+
+a = 0.3 + 0.6
+print(round(a, 4))
+
+if round(a, 4) == 0.9:
+    print(True)
+else:
+    print(False)
+
+# 수 자료형의 연산
+a = 7
+b = 3
+
+# 나누기
+print (a / b)
+
+# 나머지
+print (a % b)
+
+# 몫
+print (a // b)
+
+a = 5
+b = 3
+print(a ** b)
