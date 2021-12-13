aeafaa05c0bf828874fccd97fa54bda9da874254
y2gcoder <y2gcoder@gmail.com>
2021-12-13 23:28:46 +0900

    Learning: List

---
+++ language/string.py
@@ -0,0 +1,16 @@
+data = 'Hello World'
+print(data)
+
+data = "Don't you know \"Python\"?"
+print(data)
+
+
+a = "Hello"
+b = "World"
+print(a + " " + b)
+
+a = "String"
+print(a * 3)
+
+a = "ABCDEF"
+print(a[2:4])
