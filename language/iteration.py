8919c6f2a45eee1987b8f70a03ae1cc9a343f0af
y2gcoder <y2gcoder@gmail.com>
2021-12-14 00:06:05 +0900

    Learning: Iteration

---
+++ language/iteration.py
@@ -0,0 +1,53 @@
+i = 1
+result = 0
+
+# i가 9보다 작거나 같을 때 아래 코드를 반복적으로 실행
+while i <= 9:
+    result += i
+    i += 1
+
+print(result)
+
+
+i = 1
+result = 0
+
+# i가 9보다 작거나 같을 때 아래 코드를 반복적으로 실행
+while i <= 9:
+    if i % 2 == 1:
+        result += i
+    i += 1
+
+print(result)
+
+
+result = 0
+
+# i는 1부터 9까지의 모든 값을 순회
+for i in range(1, 10):
+    result += i
+
+print(result)
+
+
+scores = [90, 85, 77, 65, 97]
+
+for i in range(5):
+    if scores[i] >= 80:
+        print(i + 1, "번 학생은 합격입니다.")
+
+
+scores = [90, 85, 77, 65, 97]
+cheating_list = {2, 4}
+
+for i in range(5):
+    if i + 1 in cheating_list:
+        continue
+    if scores[i] >= 80:
+        print(i + 1, "번 학생은 합격입니다.")
+
+
+for i in range(2, 10):
+    for j in range(1, 10):
+        print(i, "X", j, "=", i * j)
+    print()
