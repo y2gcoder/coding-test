7e69c5dffa9693ee77ea1f97e465c9a35cf67b7a
y2gcoder <y2gcoder@gmail.com>
2021-12-14 23:18:36 +0900

    Learning: I/O

---
+++ language/io.py
@@ -0,0 +1,58 @@
+# 외워!
+# list(map(int, input().split()))
+
+
+# 데이터의 개수 입력
+# n = int(input())
+# 각 데이터를 공백으로 구분하여 입력
+# data = list(map(int, input().split()))
+
+# data.sort(reverse=True)
+# print(data)
+
+
+# n, m, k를 공백으로 구분하여 입력
+# n, m, k = map(int, input().split())
+# print(n, m, k)
+
+
+# 얘도 외워!
+# import sys
+# sys.stdin.readline().rstrip()
+
+
+# import sys
+
+# 문자열 입력받기
+# data = sys.stdin.readline().rstrip()
+# print(data)
+
+
+# 출력할 변수들
+a = 1
+b = 2
+
+print(a, b)
+
+
+# 출력할 변수들
+a = 1
+b = 2
+
+print(a)
+print(b)
+
+
+# 출력할 변수들
+# answer = 7
+#
+# print("정답은 " + answer + "입니다.") # 에러 발생
+
+
+# 출력할 변수들
+answer = 7
+
+print("정답은 " + str(answer) + "입니다.")    # 정답은 7입니다.
+print("정답은", answer, "입니다.")    # 정답은 7 입니다. (의도치 않은 공백)
+print(f"정답은 {answer}입니다.")  # f-string(>=Python 3.6)
+
