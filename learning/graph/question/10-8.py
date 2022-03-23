594777aaa991efe362dfe5ad8292030b35922479
y2gcoder <y2gcoder@gmail.com>
2022-03-23 23:50:46 +0900

    Learning: Graph ~20220323

---
+++ learning/graph/question/10-8.py
@@ -0,0 +1,43 @@
+# 특정 원소가 속한 집합을 찾기
+def find_parent(parent, x):
+    # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+    if parent[x] != x:
+        parent[x] = find_parent(parent, parent[x])
+    return parent[x]
+
+
+# 두 원소가 속한 집합을 합치기
+def union_parent(parent, a, b):
+    a = find_parent(parent, a)
+    b = find_parent(parent, b)
+    if a < b:
+        parent[b] = a
+    else:
+        parent[a] = b
+
+
+n, m = map(int, input().split())
+parent = [0] * (n + 1)
+
+edges = []
+result = 0
+max_cost = 0
+
+for i in range(1, n + 1):
+    parent[i] = i
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    edges.append((c, a, b))
+
+edges.sort()
+
+for edge in edges:
+    c, a, b = edge
+    if find_parent(parent, a) != find_parent(parent, b):
+        union_parent(parent, a, b)
+        result += c
+        if max_cost < c:
+            max_cost = c
+
+print(result - max_cost)
