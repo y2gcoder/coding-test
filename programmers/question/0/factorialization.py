55f558ababcfc36df71ad210a4da836accada017
y2gcoder <y2gcoder@gmail.com>
2022-11-14 12:11:43 +0900

    프로그래머스 소인수분해 풀이 추가

--- programmers/question/0/factorialization.py
+++ programmers/question/0/factorialization.py
@@ -6,17 +6,33 @@ https://school.programmers.co.kr/learn/courses/30/lessons/120852
 """
 
 
+# def solution(n):
+#     answer = []
+#
+#     target = 2
+#
+#     while n > 1:
+#         while n % target == 0:
+#             n = n // target
+#             answer.append(target)
+#         target += 1
+#     answer = list(set(answer))
+#     answer.sort()
+#
+#     return answer
+
+
 def solution(n):
     answer = []
 
     target = 2
 
-    while n > 1:
-        while n % target == 0:
-            n = n // target
-            answer.append(target)
-        target += 1
-    answer = list(set(answer))
-    answer.sort()
+    while target <= n:
+        if n % target == 0:
+            n //= target
+            if target not in answer:
+                answer.append(target)
+        else:
+            target += 1
 
     return answer
