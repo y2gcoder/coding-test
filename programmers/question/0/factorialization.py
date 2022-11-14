2bc9824d41d9975b71d5a4469ec56dd337666ee5
y2gcoder <y2gcoder@gmail.com>
2022-11-14 11:59:27 +0900

    프로그래머스 소인수분해 풀이

---
+++ programmers/question/0/factorialization.py
@@ -0,0 +1,22 @@
+"""
+프로그래머스
+코딩테스트 연습
+소인수분해
+https://school.programmers.co.kr/learn/courses/30/lessons/120852
+"""
+
+
+def solution(n):
+    answer = []
+
+    target = 2
+
+    while n > 1:
+        while n % target == 0:
+            n = n // target
+            answer.append(target)
+        target += 1
+    answer = list(set(answer))
+    answer.sort()
+
+    return answer
