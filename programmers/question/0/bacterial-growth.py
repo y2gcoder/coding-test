4bd09c78d2f1a76c92f9f23d777fb722ffc36794
y2gcoder <y2gcoder@gmail.com>
2022-11-17 10:14:24 +0900

    프로그래머스 세균증식 풀이

---
+++ programmers/question/0/bacterial-growth.py
@@ -0,0 +1,14 @@
+"""
+프로그래머스
+코딩테스트 연습
+세균 증식
+https://school.programmers.co.kr/learn/courses/30/lessons/120910
+"""
+
+
+def solution(n, t):
+    return n * (2 ** t)
+
+
+def solution(n, t):
+    return n << t
